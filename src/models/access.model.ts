8f237eba7b76d9e0f344747d429efcc89661d202
Atuesta257 <62776107+Atuesta257@users.noreply.github.com>
2026-04-15 13:46:04 -0500

    Delete src/models/access.model.ts

--- src/models/access.model.ts
+++
@@ -1,9 +0,0 @@
-import pool from '../config/db';
-
-export const registerAccess = async (userId: number, tipo: 'entrada' | 'salida') => {
-    const [result] = await pool.query(
-        'INSERT INTO access_logs (user_id, tipo) VALUES (?, ?)',
-        [userId, tipo]
-    );
-    return result;
-};
